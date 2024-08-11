2a1f5b88e22874be7689bb00f4672125fa6c2214
h-maren <26232423+h-maren@users.noreply.github.com>
2024-08-11 18:48:26 -0400

    Add ability to change which pic selected

--- src/chgImage.js
+++ src/chgImage.js
@@ -1,7 +1,11 @@
 const allImages=document.querySelectorAll(".pic-slide");
 const allImagesLength=allImages.length;
+const imageNav = document.querySelector(".img-nav-dots");
+const imageNavDots = imageNav.querySelectorAll("svg");
 
 let shownImageIndex=0;
+
+
 
 allImages.forEach((item,index) => {
     if(!item.classList.contains("hidden")){
@@ -35,5 +39,29 @@ let showPrevImage = (function () {
     }
 });
 
+let selectImage = (function (e) {
+    console.log(e);
+    console.log(e.target);
+    let selectedIndex=Number(e.target.dataset.index);
+    console.log(selectedIndex);
+    allImages.forEach((item,index) => {
+        console.log(item);
+        console.log(selectedIndex);
+        console.log(index);
+        console.log(item.classList.contains("hidden"));
+        console.log(index===selectedIndex);
+        if((item.classList.contains("hidden"))&&(index===selectedIndex)){
+            item.classList.remove("hidden");
+            e.target.classList.add("selected");
+            shownImageIndex=index;
+            console.log(shownImageIndex);
+        }
+        if((!item.classList.contains("hidden"))&&(index!==selectedIndex)){
+            item.classList.add("hidden");
+            imageNavDots[index].classList.remove("selected");
+        }
+    });
+});
 
-export { showNextImage, showPrevImage }
+
+export { showNextImage, showPrevImage, selectImage }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './style.css';
-import { showNextImage, showPrevImage } from './chgImage.js';
-
-let currentIndex=0;
+import { showNextImage, showPrevImage, selectImage } from './chgImage.js';
 
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
+const imageNav = document.querySelector(".img-nav-dots");
+const imageNavDots = imageNav.querySelectorAll("svg");
 
 prevBtn.addEventListener("click",showPrevImage);
 nextBtn.addEventListener("click",showNextImage);
+
+//creating index attribute for image dots
+imageNavDots.forEach((dot,index) => {
+    dot.addEventListener("click",selectImage);
+    dot.classList.add(`index-${index}`);
+    dot.setAttribute('data-index',index);
+});
